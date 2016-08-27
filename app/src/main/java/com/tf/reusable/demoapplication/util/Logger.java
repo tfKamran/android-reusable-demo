936fee8d0e0eadf6e9cddf0773b7f3f55b1678a1
Kamran <team.radiant@gmail.com>
2016-08-27 11:12:12 +0530

    Separate date and time with an underscore

--- app/src/main/java/com/tf/reusable/demoapplication/util/Logger.java
+++ app/src/main/java/com/tf/reusable/demoapplication/util/Logger.java
@@ -183,7 +183,7 @@ public class Logger {
         return calendar.get(Calendar.YEAR)
                 + "-" + calendar.get(Calendar.MONTH)
                 + "-" + calendar.get(Calendar.DAY_OF_MONTH)
-                + "-" + calendar.get(Calendar.HOUR_OF_DAY)
+                + "_" + calendar.get(Calendar.HOUR_OF_DAY)
                 + "-" + calendar.get(Calendar.MINUTE)
                 + "-" + calendar.get(Calendar.SECOND)
                 + "-" + calendar.get(Calendar.MILLISECOND);
